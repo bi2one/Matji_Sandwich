4c8c56fbd0b2cae4469aee80104247ce6f2aadf7
jung-kyoon lee <excgate@gmail.com>
2011-04-29 17:45:45 +0900

    class diagram updated.(convention canvas added, give shape to list activity)

---
+++ android/clear_script.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+rm -rf ./gen/*
+rm -rf ./bin/*
--- android/src/com/matji/sandwich/StoreListActivity.java
+++ android/src/com/matji/sandwich/StoreListActivity.java
@@ -2,14 +2,29 @@ package com.matji.sandwich;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
 
-public class StoreListActivity extends Activity
+import com.matji.sandwich.error.exception.NotPossibleMatjiException;
+
+public class StoreListActivity extends ListActivity
 {
+    private BaseAdapter adapter;
+    private HttpRequester requester;
+    private ListEventHandler eventHandler;
+    private ListHelper helper;
+    private Intent intent;
+    
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	intent = getIntent();
+	helper = (ListHelper) intent.getParcelableExtra("com.matji.sandwich.listhelper");
+	adapter = helper.getAdapter();
+	requester = helper.getRequester();
+	eventHandler = helper.getEventHandler();
     }
+
+    
 }
