d4bb670f57db07224ff5b5bd218aed5c04e598a7
jung-kyoon lee <excgate@gmail.com>
2011-05-04 18:10:16 +0900

    parser exception added.

---
+++ android/src/com/matji/sandwich/dataprovider/MatjiStorageDataProvider.java
@@ -0,0 +1,15 @@
+package com.matji.sandwich.dataprovider;
+
+import android.content.SharedPreferences;
+import android.content.Context;
+
+public class MatjiStorageDataProvider {
+    private volatile MatjiStorageDataProvider provider;
+    private Context context;
+
+    private MatjiStorageDataProvider() { }
+
+    public MatjiStorageDataProvider getInstance(Context context) {
+	
+    }
+}
--- android/src/com/matji/sandwich/http/parser/MatjiDataParser.java
+++ android/src/com/matji/sandwich/http/parser/MatjiDataParser.java
@@ -1,9 +1,10 @@
 package com.matji.sandwich.http.parser;
 
 import com.matji.sandwich.data.MatjiData;
+import com.matji.sandwich.exception.MatjiException;
 
 import java.util.ArrayList;
 
 public abstract class MatjiDataParser {
-    public abstract ArrayList<MatjiData> getData(String data);
+    public abstract ArrayList<MatjiData> getData(String data) throws MatjiException;
 }
