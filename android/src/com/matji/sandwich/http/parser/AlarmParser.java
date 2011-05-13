0a6813faaad017e10a30bbc694d3679c0984c09a
Cheol-Woo Park <mozziluv@gmail.com>
2011-05-13 20:38:47 +0900

    a

--- android/src/com/matji/sandwich/http/parser/AlarmParser.java
+++ android/src/com/matji/sandwich/http/parser/AlarmParser.java
@@ -1,43 +1,23 @@
 package com.matji.sandwich.http.parser;
 
-import java.util.ArrayList;
-
+import com.google.gson.JsonObject;
 import com.matji.sandwich.data.Alarm;
 import com.matji.sandwich.data.MatjiData;
+import com.matji.sandwich.data.User;
 import com.matji.sandwich.exception.MatjiException;
-import com.matji.sandwich.exception.JSONMatjiException;
-import com.matji.sandwich.json.MatjiJSONArray;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-
-public class AlarmParser extends MatjiDataParser{
-    public ArrayList<MatjiData> getRawData(String data) throws MatjiException {
-	ArrayList<MatjiData> alarmList = new ArrayList<MatjiData>();
-	MatjiJSONArray jsonArray;
-	try {
-	    jsonArray = new MatjiJSONArray(data);
-	    JSONObject element;
-	    alarmList.clear();
-		for(int i=0 ; i < jsonArray.length() ; i++){
-		    element = jsonArray.getMatjiJSONObject(i);
-		    Alarm alarm = new Alarm();
-		    alarm.setId(element.getString("id"));
-		    alarm.setReceivedUserId(element.getString("received_user_id"));
-		    alarm.setSentUserId(element.getString("sent_user_id"));
-		    alarm.setAlarmType(element.getString("alarm_type"));
-		    alarmList.add(alarm);
-		}
-	} catch (JSONException e1) {
-	    // TODO Auto-generated catch block
-	    e1.printStackTrace();
+public class AlarmParser extends MatjiDataParser {
+	protected MatjiData getMatjiData(JsonObject object) throws MatjiException {
+		Alarm alarm = new Alarm();
+		alarm.setId(getInt(object, "id"));
+		alarm.setReceivedUserId(getInt(object, "received_user_id"));
+		alarm.setSentUserId(getInt(object, "sent_user_id"));
+		alarm.setAlarmType(getString(object, "alarm_type"));
+		alarm.setSentUser((User) new UserParser().getRawObject(getString(object, "sent_user")));
+		alarm.setReceivedUser((User) new UserParser().getRawObject(getString(object, "received_user")));
+		alarm.setCreatedAt(getString(object, "created_at"));
+		alarm.setUpdatedAt(getString(object, "updated_at"));
+		
+		return alarm;
 	}
-	return alarmList;
-    }
-
-    public ArrayList<MatjiData> getData(String data) throws MatjiException {
-    	String validData = validateData(data);
-       	return getRawData(validData);
-    }
 }
