663a4dbb7e40a51d5a5197af4ab6fa9202d775ff
Jihun, Park <brandnew@matji.com>
2011-05-17 03:36:44 +0900

    DataProvider worked

--- android/src/com/matji/sandwich/FlipperTestActivity.java
+++ android/src/com/matji/sandwich/FlipperTestActivity.java
@@ -7,8 +7,8 @@ import android.content.Context;
 import android.view.MotionEvent;
 import android.util.Log;
 import android.view.WindowManager;
-import com.matji.sandwich.widget.TestListView;
-import com.matji.sandwich.widget.SwipeView;
+//import com.matji.sandwich.widget.TestListView;
+//import com.matji.sandwich.widget.SwipeView;
 import android.view.ViewGroup.LayoutParams;
 
 // import com.matji.sandwich.widget.SwipeView;
@@ -18,18 +18,18 @@ public class FlipperTestActivity extends Activity {
 	setContentView(R.layout.flipper);
 	Context context = getApplicationContext();
 
-	SwipeView sv = (SwipeView)findViewById(R.id.SwipeView);
-	TestListView test = (TestListView)findViewById(R.id.TestListView);
-	TestListView test2 = (TestListView)findViewById(R.id.TestListView2);
-	TestListView test3 = (TestListView)findViewById(R.id.TestListView3);
+//	SwipeView sv = (SwipeView)findViewById(R.id.SwipeView);
+//	TestListView test = (TestListView)findViewById(R.id.TestListView);
+//	TestListView test2 = (TestListView)findViewById(R.id.TestListView2);
+//	TestListView test3 = (TestListView)findViewById(R.id.TestListView3);
 
-	test.start(this);
-	test2.start(this);
-	test3.start(this);
-
-	TestListView addTest = new TestListView(context, null);
-	sv.addView(addTest);
-	addTest.start(this);
+//	test.start(this);
+//	test2.start(this);
+//	test3.start(this);
+//
+//	TestListView addTest = new TestListView(context, null);
+//	sv.addView(addTest);
+//	addTest.start(this);
 	
 	// get window width
 	// WindowManager mWinMgr = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
