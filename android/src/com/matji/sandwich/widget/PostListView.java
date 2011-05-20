2133501202a21afb4e76ab4d0c89573b027efc2d
asky <asky@asky.asky>
2011-05-20 19:12:33 +0900

    asdads

--- android/src/com/matji/sandwich/widget/PostListView.java
+++ android/src/com/matji/sandwich/widget/PostListView.java
@@ -4,28 +4,35 @@ import java.util.ArrayList;
 
 import android.app.*;
 import android.content.Context;
-import android.content.Intent;
-import android.util.AttributeSet;
+import android.util.*;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.*;
 
 import com.matji.sandwich.*;
-import com.matji.sandwich.data.Post;
 import com.matji.sandwich.data.MatjiData;
 import com.matji.sandwich.adapter.PostAdapter;
 import com.matji.sandwich.exception.MatjiException;
 import com.matji.sandwich.http.request.HttpRequest;
 import com.matji.sandwich.http.request.PostHttpRequest;
 
-public class PostListView extends RequestableMListView {
+public class PostListView extends RequestableMListView implements OnClickListener {
 	private PostHttpRequest postRequest;
 	
 	public PostListView(Context context, AttributeSet attrs) {
 		super(context, attrs, new PostAdapter(context), 10);
 		postRequest = new PostHttpRequest(context);
 		setPage(1);
+		
+	 
 	}
 
 	public void start(Activity activity) {
 		super.start(activity);
+//		TextView storeNameView = (TextView)findViewById(R.id.post_adapter_store_name);
+//		TextView nickView = (TextView)findViewById(R.id.post_adapter_nick);
+//		storeNameView.setOnClickListener(this);
+//		nickView.setOnClickListener(this);
 	}
 
 	public HttpRequest request() {
@@ -49,4 +56,12 @@ public class PostListView extends RequestableMListView {
 		MainTabActivity tabAct = MainTabActivity.class.cast( getActivity().getParent());
 		tabAct.getTabHost().setCurrentTab(1);
 	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
