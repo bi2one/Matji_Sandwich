01e75a106e1952f53f5c64b5ee8a95a3005c7641
asky <asky@asky.asky>
2011-05-20 16:34:55 +0900

    Sadf

---
+++ android/src/com/matji/sandwich/PostInfoActivity.java
@@ -0,0 +1,49 @@
+package com.matji.sandwich;
+
+import java.util.ArrayList;
+
+import com.matji.sandwich.data.Post;
+import com.matji.sandwich.data.Tag;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PostInfoActivity extends Activity {	
+	Post post;
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.store_info);		
+
+		setStoreInfo();
+	}
+
+	private void setStoreInfo() {
+		Intent intent = getIntent();
+		post = intent.getParcelableExtra("store");
+
+		TextView likeCountText = (TextView) findViewById(R.id.store_info_like_store_count);
+		TextView addressText = (TextView) findViewById(R.id.store_info_store_address);
+		TextView coverText = (TextView) findViewById(R.id.store_info_cover);
+		TextView popularMenuText = (TextView) findViewById(R.id.store_info_popular_menu);
+		TextView tagsText = (TextView) findViewById(R.id.store_info_tag);
+		TextView diggerText = (TextView) findViewById(R.id.store_info_tag);
+		Button postCountButton = (Button)findViewById(R.id.store_info_memo_button);
+		Button imageCountButton = (Button)findViewById(R.id.store_info_image_button);
+		Button tagCountButton = (Button)findViewById(R.id.store_info_tag_button);
+		Button urlButton = (Button)findViewById(R.id.store_info_url_button);
+
+		// TODO  테스트용
+		popularMenuText.setText("인기메뉴: TODO");
+//		if (store.getTagCount() > 0) {
+//			tagsText.setText("태그: " + tags.get(0).getTag());
+//			for (int i = 1; i < ((tags.size() > 5) ? 5 : tags.size()); i++) {
+//				tagsText.setText(tagsText.getText() + ", " + tags.get(i).getTag());
+//			}
+//		}
+	}
+}
---
+++ android/src/com/matji/sandwich/widget/PostListView.java
@@ -0,0 +1,50 @@
+package com.matji.sandwich.widget;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+
+import com.matji.sandwich.PostInfoActivity;
+import com.matji.sandwich.data.Post;
+import com.matji.sandwich.data.MatjiData;
+import com.matji.sandwich.adapter.PostAdapter;
+import com.matji.sandwich.exception.MatjiException;
+import com.matji.sandwich.http.request.HttpRequest;
+import com.matji.sandwich.http.request.PostHttpRequest;
+
+public class PostListView extends RequestableMListView {
+	private PostHttpRequest postRequest;
+	
+	public PostListView(Context context, AttributeSet attrs) {
+		super(context, attrs, new PostAdapter(context), 10);
+		postRequest = new PostHttpRequest(context);
+		setPage(1);
+	}
+
+	public void start(Activity activity) {
+		super.start(activity);
+	}
+
+	public HttpRequest request() {
+		postRequest.actionList(getPage(), getLimit());
+		return postRequest;
+	}
+
+	public void requestCallBack(int tag, ArrayList<MatjiData> data) {
+		super.requestCallBack(tag, data);
+	}
+	
+	public void requestExceptionCallBack(int tag, MatjiException e) {
+		super.requestExceptionCallBack(tag, e);
+	    }
+
+	public void onListItemClick(int position) {
+		Post post = (Post)(getAdapterData().get(position));
+		Intent intent = new Intent(getActivity(), PostInfoActivity.class);
+		intent.putExtra("post", post);
+		getActivity().startActivity(intent);
+	}
+}
