b584367f056e5087a6372c33908f6ac01da4df1b
jung-kyoon lee <excgate@gmail.com>
2011-05-23 15:52:00 +0900

    image downloader added.

--- android/src/com/matji/sandwich/http/request/HttpRequest.java
+++ android/src/com/matji/sandwich/http/request/HttpRequest.java
@@ -25,7 +25,7 @@ public abstract class HttpRequest {
 	protected Hashtable<String, String> getHashtable;	
 	protected HttpMethod httpMethod;
 	
-		
+
 	@SuppressWarnings("unused")
 	private HttpRequest(){}
 	
@@ -33,7 +33,6 @@ public abstract class HttpRequest {
 		this.context = context;
 		postHashtable = new Hashtable<String, Object>();
 		getHashtable = new Hashtable<String, String>();
-		
 	}
 	
 	
@@ -55,12 +54,10 @@ public abstract class HttpRequest {
 						   Map<String, String> getParam,
 						   int method) throws HttpConnectMatjiException {
 	SimpleHttpResponse httpResponse = null;
-	// �ㅽ�����낫 �산린
 	ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
 	NetworkInfo netInfo_mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 	NetworkInfo netInfo_wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 		
-	// �ㅽ������ �щ� ���
 	if((netInfo_mobile.getState() == NetworkInfo.State.CONNECTED) ||
 	   (netInfo_wifi.getState() == NetworkInfo.State.CONNECTED)) {
 	    Map<String,String> baseHeader = new HashMap<String,String>();
