2d631df6d70a56ae4d22196e13d844bd5a16d14d
Cheol-Woo Park <mozziluv@gmail.com>
2011-06-08 15:29:32 +0900

    a

---
+++ android/src/com/matji/sandwich/MainActivity.java
@@ -0,0 +1,16 @@
+package com.matji.sandwich;
+
+import android.app.Activity;
+
+public abstract class MainActivity extends Activity {
+	public abstract void initInfo();
+	public abstract void setInfo();	
+	
+	public String getCount(int id, int count) {
+		return getString(id) + ": " + count + getString(R.string.default_string_number_of);
+	}
+	
+	public String getCountNumberOf(int id, int count) {
+		return getString(id) + ": " + count + getString(R.string.default_string_number_of);
+	}
+}
