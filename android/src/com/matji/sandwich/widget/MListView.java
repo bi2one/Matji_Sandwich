abcfbc6f754e19ef85ec7f6dbad2567e00a68712
Jihun, Park <brandnew@matji.com>
2011-06-13 17:23:55 +0900

    asd

--- android/src/com/matji/sandwich/widget/MListView.java
+++ android/src/com/matji/sandwich/widget/MListView.java
@@ -1,6 +1,7 @@
 package com.matji.sandwich.widget;
 
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.AdapterView;
@@ -9,38 +10,46 @@ import android.widget.LinearLayout;
 import android.widget.AdapterView.OnItemClickListener;
 import android.app.Activity;
 
+// import com.matji.sandwich.http.HttpRequestManager;
+import com.matji.sandwich.R;
+
 public abstract class MListView extends ListView implements OnItemClickListener {
-	private Activity activity;
-	private LinearLayout header;
-	public abstract void onListItemClick(int position);
+    private Activity activity;
+    private LinearLayout header;
+    public abstract void onListItemClick(int position);
 
-	public MListView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		header = new LinearLayout(context);
-		header.setOrientation(LinearLayout.VERTICAL);
-		super.addHeaderView(header, null, false);
-		setOnItemClickListener(this);
-	}
+    public MListView(Context context, AttributeSet attrs) {
+	super(context, attrs);
+	header = new LinearLayout(context);
+	header.setOrientation(LinearLayout.VERTICAL);
 
-	public void addHeaderView(View v) {
-		header.addView(v, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-	}
+	super.addHeaderView(header, null, false);
+	setOnItemClickListener(this);
+    }
 
-	public void addHeaderView(ViewContainer container) {
-		addHeaderView(container.getRootView());
-	}
+    public void addHeaderView(View v) {
+	header.addView(v, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+    }
 
-	public Activity getActivity() {
-		return activity;
-	}
+    public void addHeaderView(ViewContainer container) {
+	addHeaderView(container.getRootView());
+    }
+    
+    public LinearLayout getHeaderViewContainer(){
+    	return header;
+    }
 
-	public void setActivity(Activity activity) {
-		this.activity = activity;
-	}
+    public Activity getActivity() {
+	return activity;
+    }
 
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		if ((position - 1) >= 0) {
-			onListItemClick(position - 1);
-		}
+    public void setActivity(Activity activity) {
+	this.activity = activity;
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	if ((position - 1) >= 0) {
+	    onListItemClick(position - 1);
 	}
+    }
 }
