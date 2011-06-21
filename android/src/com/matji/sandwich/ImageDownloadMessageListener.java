f89bc43059cf0aec9668d0b8af342d2aa4e2f21c
Cheol-Woo Park <mozziluv@gmail.com>
2011-06-21 17:49:08 +0900

    add write comment activity

---
+++ android/src/com/matji/sandwich/ImageDownloadMessageListener.java
@@ -0,0 +1,7 @@
+package com.matji.sandwich;
+
+import android.widget.ImageView;
+
+public interface ImageDownloadMessageListener {
+	public void onMessageDelivered(int msg, ImageView v);
+}
---
+++ android/src/com/matji/sandwich/StoreBookmarkedListActivity.java
@@ -0,0 +1,40 @@
+package com.matji.sandwich;
+
+import com.matji.sandwich.base.BaseActivity;
+import com.matji.sandwich.widget.StoreBookmarkedListView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StoreBookmarkedListActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_store);
+
+		int user_id = getIntent().getIntExtra("user_id", 0);
+
+		StoreBookmarkedListView listView = (StoreBookmarkedListView) findViewById(R.id.store_bookmarked_list);
+		listView.setUserId(user_id);
+		listView.setActivity(this);
+		listView.requestReload();
+	}
+
+	@Override
+	protected String titleBarText() {
+		return "UserStoreListActivity";
+	}
+
+	@Override
+	protected boolean setTitleBarButton(Button button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void onTitleBarItemClicked(View view) {
+		// TODO Auto-generated method stub
+		
+	}
+}
