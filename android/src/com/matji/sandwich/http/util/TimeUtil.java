0a88ea7a877ca899c0424c8bfa21871634fe2d52
Cheol-Woo Park <mozziluv@gmail.com>
2011-06-22 15:55:52 +0900

    notice

---
+++ android/src/com/matji/sandwich/http/util/TimeUtil.java
@@ -0,0 +1,94 @@
+package com.matji.sandwich.http.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class TimeUtil {
+	final static String timezone = "UTC";	
+	static int timeOffset = 0;
+	
+	public static String getAgoFromSecond(long second) {
+		long x = 0;		
+		String timestampText;
+		String year = "year ago";
+		String years = "years ago";
+		String month = "month ago";
+		String months = "months ago";
+		String week = "week ago";
+		String weeks = "weeks ago";
+		String day = "day ago";
+		String days = "days ago";
+		String hour = "hour ago";
+		String hours = "hours ago";
+		String min = "min ago";
+		String sec = "sec ago";
+		
+		if ((x = second / 31104000) > 0) { // year
+			if (x == 1) {
+				timestampText = x + " " + year; // singular
+			} else {
+				timestampText = x + " " + years;	// plural
+			}
+		} else {
+			if ((x = second / 2592000) > 0) { // month
+				if (x == 1) {
+					timestampText = x+ " " + month; // singular
+				} else {
+					timestampText = x+ " " + months; // plural
+				}
+			} else {
+				if ((x = second / 604800) > 0) { // week
+					if (x == 1) {
+						timestampText = x+ " " + week; // singular
+					} else {
+						timestampText = x+ " " + weeks; // plural
+					}
+				} else {
+					if ((x = second / 86400) > 0) { // day
+						if (x == 1) {
+							timestampText = x+ " " + day; // singular
+						} else {
+							timestampText = x+ " " + days; // plural
+						}
+					} else {
+						if ((x = second / 3600) > 0) { // hour
+							if (x == 1) {
+								timestampText = x+ " " + hour; // singular
+							} else {
+								timestampText = x+ " " + hours; // plural
+							}
+						} else {
+							if ((x = second / 60) > 0) { // min
+								timestampText = x+ " " + min;
+							} else { // sec
+								timestampText = second+ " " + sec;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return timestampText;
+	}
+	
+	public static Date getDateInTimeZone(Date currentDate, String timeZoneId)
+	{
+		TimeZone tz = TimeZone.getTimeZone(timeZoneId);
+		Calendar mbCal = new GregorianCalendar(tz);
+		mbCal.setTimeInMillis(currentDate.getTime());
+	
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, mbCal.get(Calendar.YEAR));
+		cal.set(Calendar.MONTH, mbCal.get(Calendar.MONTH));
+		cal.set(Calendar.DAY_OF_MONTH, mbCal.get(Calendar.DAY_OF_MONTH));
+		cal.set(Calendar.HOUR_OF_DAY, mbCal.get(Calendar.HOUR_OF_DAY));
+		cal.set(Calendar.MINUTE, mbCal.get(Calendar.MINUTE));
+		cal.set(Calendar.SECOND, mbCal.get(Calendar.SECOND));
+		cal.set(Calendar.MILLISECOND, mbCal.get(Calendar.MILLISECOND));
+	
+		return cal.getTime();
+	}
+}
---
+++ android/src/com/matji/sandwich/widget/NoticeListView.java
@@ -0,0 +1,68 @@
+package com.matji.sandwich.widget;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.matji.sandwich.R;
+import com.matji.sandwich.adapter.NoticeAdapter;
+import com.matji.sandwich.data.MatjiData;
+import com.matji.sandwich.data.Notice;
+import com.matji.sandwich.http.request.HttpRequest;
+import com.matji.sandwich.http.request.NoticeHttpRequest;
+
+public class NoticeListView extends RequestableMListView {
+	private HttpRequest request;
+	private Context context;
+
+	public NoticeListView(Context context, AttributeSet attrs) {
+		super(context, attrs, new NoticeAdapter(context), 10);
+		this.context = context;
+
+		setPage(1);
+	}
+
+	public HttpRequest request() {
+		if (request == null || !(request instanceof NoticeHttpRequest)) {
+			request = new NoticeHttpRequest(getActivity());
+		}
+
+		((NoticeHttpRequest) request).actionList(getPage(), getLimit());
+
+		return request;
+	}
+
+	public void onListItemClick(int position) {
+		Log.d("Matji", "ListItemClicked");
+		((NoticeAdapter) getMBaseAdapter()).extendNotice(position);
+	}
+
+	@Override
+	public void requestCallBack(int tag, ArrayList<MatjiData> data) {
+		String target = null;
+		String targetAndroid = context.getString(R.string.target_android);
+		String targetCommon = context.getString(R.string.target_common);
+
+		ArrayList<MatjiData> removedData = new ArrayList<MatjiData>();
+		
+		for (MatjiData d : data) {
+			target = ((Notice) d).getTarget();
+			
+			if (target != null) {
+				if (!(target.equals(targetAndroid) || target.equals(targetCommon))) {
+					removedData.add(d);
+				}
+			}  else {
+				removedData.add(d);
+			}
+		}
+		
+		for (MatjiData d : removedData) {
+			data.remove(d);
+		}
+
+		super.requestCallBack(tag, data);
+	}
+}
