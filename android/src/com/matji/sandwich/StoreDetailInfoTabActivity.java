de2cbfdaa613b96762b8ce18d0bd496fbf4d47f4
asky <asky@asky.asky>
2011-07-29 14:12:26 +0900

    store main

--- android/src/com/matji/sandwich/StoreDetailInfoTabActivity.java
+++ android/src/com/matji/sandwich/StoreDetailInfoTabActivity.java
@@ -1,29 +1,19 @@
 package com.matji.sandwich;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TabHost;
 
 import com.matji.sandwich.base.BaseTabActivity;
+import com.matji.sandwich.widget.RoundTabHost;
 
 public class StoreDetailInfoTabActivity extends BaseTabActivity {
-	private Intent defaultIntent;
-	private Intent noteIntent;
+	private RoundTabHost tabHost;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		defaultIntent = new Intent(this, StoreDefaultInfoActivity.class);
-		noteIntent = new Intent(this, StoreNoteListActivity.class);
+		tabHost = (RoundTabHost)getTabHost();
 		
-		TabHost tabHost = getTabHost();
-		
-		tabHost.addTab(tabHost.newTabSpec("tab1")
-				.setIndicator(getString(R.string.store_detail_info_default))
-				.setContent(defaultIntent));
-		tabHost.addTab(tabHost.newTabSpec("tab2")
-				.setIndicator(getString(R.string.store_detail_info_note))
-				.setContent(noteIntent));
-	}
+
+	}	
 }
